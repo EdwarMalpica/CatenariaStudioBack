58f9716bb2cf58551465a0b47a4d7e4cca4d6759
EdwarMalpica <edwar.malpica@uptc.edu.co>
2023-09-17 11:40:13 -0500

    Correccion entidad Citas

--- app/Models/Citas.php
+++ app/Models/Citas.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Citas extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'fecha_cita',
+        'mensaje',
+        'user_id'
+    ];
 }
